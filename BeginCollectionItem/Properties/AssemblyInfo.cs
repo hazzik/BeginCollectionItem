32aa88038ebe99c5d00d7de6827c371f6275133c
Alexander I. Zaytsev <hazzik@gmail.com>
2013-03-17 22:53:58 +1300

    Sign the assembly

--- BeginCollectionItem/Properties/AssemblyInfo.cs
+++ BeginCollectionItem/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9e5ccf6d-6873-4da5-bf76-49a4e693accc")]
-[assembly:InternalsVisibleTo("BeginCollectionItem.Tests")]
+[assembly:InternalsVisibleTo("BeginCollectionItem.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010005873469e55b5c080f3bba3e7cbd730d37d33f4163c61ac77d12be4b77841dbb21543d6bdaf984e8461a315598c701124ee5458778cbbc5299668418f9f236377ba1c231d2ac2e61f23a498e1c298b5b11526a502bb0ec346b92b92bc64bfbf2520b838b3d2fbb72ceb27e6dba43a1d5de0896410979131a1c379ae3a30110f8")]
 
 // Version information for an assembly consists of the following four values:
 //
